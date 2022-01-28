556dd2e8bdf992fb05de38b3b4becc70f5729dcc
Melvin Idema <melvin_i13@hotmail.com>
2022-01-28 11:30:33 +0100

    Add correct logo

--- src/style.js
+++ src/style.js
@@ -3,8 +3,8 @@ import styled from "styled-components";
 export const Logo = styled.div`
   position: absolute;
   top: 30px; left: 30px;
-  width: 150px; height: 150px;
-  background-image: url("logo.svg");
+  width: 200px; height: 150px;
+  background-image: url("logo.png");
   background-repeat: no-repeat;
   background-size: cover;
 `
